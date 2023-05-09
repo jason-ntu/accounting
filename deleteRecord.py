b94ba7230726095f40098548ebae104a9e86db02
eliawang <eliawang.cs07@nycu.edu.tw>
2023-05-09 17:09:39 +0800

    record feature function all DONE

--- deleteRecord.py
+++ deleteRecord.py
@@ -2,12 +2,7 @@ from enum import IntEnum, auto
 from datetime import datetime
 import sqlalchemy as sql
 from accessor import Accessor
-
-
-class DeleteRecordOption(IntEnum):
-    ALL = auto()
-    DATE = auto()
-    BACK = auto()
+from readRecord import ReadRecordPage, ReadRecordOption
 
 class DeleteRecordPage(Accessor):
 
@@ -15,32 +10,6 @@ class DeleteRecordPage(Accessor):
     table_name = "Record"
     IDerrorMsg = "輸入的ID須為整數"
 
-    def show(self):
-        print("%d: 查看所有紀錄ID" % DeleteRecordOption.ALL)
-        print("%d: 查看指定日期紀錄ID" % DeleteRecordOption.DATE)
-        print("%d: 回到上一頁" % DeleteRecordOption.BACK)
-
-    def choose(self):
-        while True:
-            try:
-                option = DeleteRecordOption(int(input()))
-                break
-            except ValueError:
-                print(self.errorMsg)
-        return option
-
-    def execute(self,option):
-        if option is DeleteRecordOption.ALL:
-            self.deleteByAll()
-        elif option is DeleteRecordOption.DATE:
-            self.deleteByDate()
-        else:
-            raise ValueError(self.errorMsg)
-    
-    def format_print(self, results):
-        for row in results:
-            dictRow = row._asdict()
-            print(dictRow['id'], "\t", dictRow['category'],"\t",dictRow['amount'],"\t", dictRow['payment'],"\t", dictRow['place'].decode("utf-8"),"\t", dictRow['time'])
             
     def checkIDInteger(self):
         while True:
@@ -61,30 +30,16 @@ class DeleteRecordPage(Accessor):
             print("此紀錄ID不存在")
         self.tearDown_connection()
 
-    def deleteByAll(self):
-        self.setUp_connection_and_table()
-        query = sql.select(self.table)
-        results = self.conn.execute(query).fetchall()
-        self.tearDown_connection()
-        self.format_print(results)
-        self.deleteByID()
-
-    def deleteByDate(self):
-        Date = input("請輸入想要看的日期(yyyy-mm-dd): ")
-        self.setUp_connection_and_table()
-        query = sql.select(self.table).where(self.table.c.time == Date)
-        results = self.conn.execute(query).fetchall()
-        self.tearDown_connection()
-        self.format_print(results)
-        self.deleteByID()
 
     def start(self):
         while True:
-            self.show()
-            option = self.choose()
-            if option is DeleteRecordOption.BACK:
+            readRecordPage = ReadRecordPage()
+            readRecordPage.show()
+            option = readRecordPage.choose()
+            if option is ReadRecordOption.BACK:
                 return
-            self.execute(option)
+            readRecordPage.execute(option)
+            self.deleteByID()
 
 if __name__ == '__main__':  # pragma: no cover
     deleteRecordPage = DeleteRecordPage()
