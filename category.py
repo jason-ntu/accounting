44135c77dd5f2b4e292cd2bbd5daab7988ca38d2
Jason <df41022@gmail.com>
2023-05-11 18:50:53 +0800

    categoryTest

--- category.py
+++ category.py
@@ -1,6 +1,17 @@
+from enum import IntEnum, auto
+import sqlalchemy as sql
+from accessor import Accessor, ExecutionStatus as es
+import const
 
-class Category:
+class CategoryOption(IntEnum):
+    CREATE = auto()
+    READ = auto()
+    UPDATE = auto()
+    DELETE = auto()
+    BACK = auto()
 
+
+class CategoryPage(Accessor):
     def create():
         pass
 
