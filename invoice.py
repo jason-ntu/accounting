5fe2970dfbf65be29f49b4cca67c34bc0f11c46e
stuSylvia <ad.sylviata@gmail.com>
2023-05-22 20:28:14 +0800

    delete some import codes in invoice.py

--- invoice.py
+++ invoice.py
@@ -1,10 +1,4 @@
-import datetime
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromiumService
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 class InvoicePage:
     #進入點
