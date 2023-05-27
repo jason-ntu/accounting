15d28df5c2931231b270f7bf4c2ae779c77d1844
stuSylvia <ad.sylviata@gmail.com>
2023-05-27 22:59:23 +0800

    change for PR

--- invoice.py
+++ invoice.py
@@ -26,7 +26,6 @@ class InvoiceText():
 class InvoicePage(Accessor):
     table_name = "Record"
     dicLatest = {}
-    # dicLatest = {'period': '112年01-02月中獎號碼單', 'award1': '06634385', 'award2': '66882140', 'award3': ['25722152', '93412693', '16957025']}
     
     # 進入點
     def start(self):
@@ -73,35 +72,35 @@ class InvoicePage(Accessor):
     
             #月份
             xpathPeriod = "//li[1]/a[@class='etw-on']"
-            htmlResult = WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpathPeriod)))
+            WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpathPeriod)))
             strPeriod = browser.find_element("xpath", xpathPeriod)
             self.dicLatest["period"] = strPeriod.text
     
             #特別獎
             xpathAward1 = "//div[@class='etw-web']/table[@class='etw-table-bgbox etw-tbig']/tbody/tr[1]/td[2]/p[@class='etw-tbiggest']/span[@class='font-weight-bold etw-color-red']"
-            htmlResult = WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpathAward1)))
+            WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpathAward1)))
             strNumber1 = browser.find_element("xpath", xpathAward1)
             self.dicLatest["award1"] = strNumber1.text
     
             #特獎
             xpathAward2 = "//div[@class='etw-web']/table[@class='etw-table-bgbox etw-tbig']/tbody/tr[2]/td[2]/p[@class='etw-tbiggest']/span[@class='font-weight-bold etw-color-red']"
-            htmlResult = WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpathAward2)))
+            WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpathAward2)))
             strNumber2 = browser.find_element("xpath", xpathAward2)
             self.dicLatest["award2"] = strNumber2.text
     
             #頭獎
             xpathAward3_1 = "/html/body/div[@class='etw-page']/div[@class='etw-wrapper']/div[@id='etw-container']/div[@class='container-xl']/div[@class='container-fluid etw-bgbox mb-4']/div[@class='etw-web']/table[@class='etw-table-bgbox etw-tbig']/tbody/tr[3]/td[2]/p[@class='etw-tbiggest mb-md-4'][1]"
-            htmlResult = WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpathAward3_1)))
+            WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpathAward3_1)))
             strNumber3_1 = browser.find_element("xpath", xpathAward3_1)
             aryAward3.append(strNumber3_1.text.strip())
     
             xpathAward3_2 = "/html/body/div[@class='etw-page']/div[@class='etw-wrapper']/div[@id='etw-container']/div[@class='container-xl']/div[@class='container-fluid etw-bgbox mb-4']/div[@class='etw-web']/table[@class='etw-table-bgbox etw-tbig']/tbody/tr[3]/td[2]/p[@class='etw-tbiggest mb-md-4'][2]"
-            htmlResult = WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpathAward3_2)))
+            WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpathAward3_2)))
             strNumber3_2 = browser.find_element("xpath", xpathAward3_2)
             aryAward3.append(strNumber3_2.text.strip())
     
             xpathAward3_3 = "/html/body/div[@class='etw-page']/div[@class='etw-wrapper']/div[@id='etw-container']/div[@class='container-xl']/div[@class='container-fluid etw-bgbox mb-4']/div[@class='etw-web']/table[@class='etw-table-bgbox etw-tbig']/tbody/tr[3]/td[2]/p[@class='etw-tbiggest mb-md-4'][3]"
-            htmlResult = WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpathAward3_3)))
+            WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpathAward3_3)))
             strNumber3_3 = browser.find_element("xpath", xpathAward3_3)
             aryAward3.append(strNumber3_3.text.strip())
 
@@ -125,10 +124,10 @@ class InvoicePage(Accessor):
         year = int(period[0:3]) + 1911
         month1 = int(period[4:6])
         month2 = int(period[7:9])
-        dateS = datetime(year, month1, 1)
-        dateE = datetime(year, month2, calendar.monthrange(year, month2)[1])        
+        dateStart = datetime(year, month1, 1)
+        dateEnd = datetime(year, month2, calendar.monthrange(year, month2)[1])        
 
-        records = self.queryRecord(dateS, dateE)        
+        records = self.queryRecord(dateStart, dateEnd)        
         if records:
             headerE = ["id", "IE", "category", "account", "amount", "location", "purchaseDate", "debitDate", "invoice", "note"]
             headerC = ["id", "收/支", "類別", "金額", "帳戶", "地點", "消費時間", "扣款時間", "發票號碼", "備註"]
@@ -138,8 +137,8 @@ class InvoicePage(Accessor):
             headerC.append("兌獎結果")
             df2 = pd.DataFrame(columns = headerE)
 
-            for index, row in df.iterrows():
-                ret = self.doPair(row["invoice"])
+            for row in df.iterrows():
+                ret = self.checkNumbers(row["invoice"])
                 if ret != '':
                     row["result"] =  ret
                     df2.loc[len(df2)] = row
@@ -149,7 +148,7 @@ class InvoicePage(Accessor):
             print("\n")
 
     # 兌獎邏輯
-    def doPair(self, myInvoice):
+    def checkNumbers(self, myInvoice):
         ret = ''
         if myInvoice == self.dicLatest["award1"]: ret = '對中 1000 萬元！'   # 對中特別獎
         if myInvoice == self.dicLatest["award2"]: ret = '對中 200 萬元！'    # 對中特獎
@@ -159,19 +158,19 @@ class InvoicePage(Accessor):
             if myInvoice == i:
                 ret = '對中 20 萬元！'   # 對中頭獎
                 break
-            if myInvoice[-7:] == i[-7:]:
+            elif myInvoice[-7:] == i[-7:]:
                 ret = '對中 4 萬元！'    # 末七碼相同
                 break
-            if myInvoice[-6:] == i[-6:]:
+            elif myInvoice[-6:] == i[-6:]:
                 ret = '對中 1 萬元！'    # 末六碼相同
                 break
-            if myInvoice[-5:] == i[-5:]:
+            elif myInvoice[-5:] == i[-5:]:
                 ret = '對中 4000 元！'   # 末五碼相同
                 break
-            if myInvoice[-4:] == i[-4:]:
+            elif myInvoice[-4:] == i[-4:]:
                 ret = '對中 1000 元！'   # 末四碼相同
                 break
-            if myInvoice[-3:] == i[-3:]:
+            elif myInvoice[-3:] == i[-3:]:
                 ret = '對中 200 元！'    # 末三碼相同
                 break
         
