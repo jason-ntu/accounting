8b3728b766a1d1438df20c700e14a6d21c219ec0
eliawang <eliawang.cs07@nycu.edu.tw>
2023-05-28 23:00:11 +0800

    delete some comment

--- recordsTest.py
+++ recordsTest.py
@@ -245,12 +245,4 @@ class TestAccountPage(MockDB):
             _stdout.truncate(0)
             _stdout.seek(0)
 
-    # @patch("sys.stdout", new_callable=io.StringIO)
-    # def test_updateAccountAmount(self, _stdout):
-    #     with self.mock_db_config:
-    #         RecordPage.setUp_connection_and_table(["Record"])
-    #         RecordPage.updateAccountAmount("EXPENSE", "現金", 100)
-    #         query = sql.select(RecordPage.tables[0].c['amount']).where(RecordPage.tables[0].c.account == "現金")
-    #         result = RecordPage.conn.execute(query).fetchone()
-    #         RecordPage.tearDown_connection(es.NONE)
-        
+    
